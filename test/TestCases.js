0d9f673869023e49f86bf6d64f53ef9fd8f2f88e
Jevan Chan <jevan.cnchan@gmail.com>
2019-01-30 13:01:26 +0800

    test: add single-user-test and multiple-user-test

--- test/TestCases.js
+++ test/TestCases.js
@@ -5,81 +5,93 @@ const fs = require("fs");
 const net = require("net");
 const child_process = require("child_process");
 const util = require("util");
+const yaml = require("js-yaml");
+const delayClient = require("./lib/DelayClient");
 
-const execPath = path.resolve("npshell_server");
-const workSpaceDir = path.resolve("test/_workSpace");
+const ExecPath = path.resolve("npserver_single_proc");
+const WorkspaceDir = path.resolve("test/_workspace");
+const CommandDelayTime = 250;
 
 let serverPortStart = 12300;
 
-describe("TestCases", () => {
-  shell.cd("test");
+describe("TestCases", function() {
+  shell.cd(__dirname);
 
   shell
-    .ls("-l", "cases")
-    .filter(c => c && c.name.charAt(0) != "_")
-    .forEach(item => {
-      describe(item.name, () => {
-        let testItemDir = path.resolve("cases", item.name);
-        shell.mkdir("-p", path.join(testItemDir, "_workSpaceTemplate"));
+    .ls("-l", "test-cases")
+    .filter(g => g && g.name.charAt(0) != "_" && g.isDirectory())
+    .forEach(group => {
+      describe(group.name, function() {
+        let groupDir = path.resolve("test-cases", group.name);
+        shell.mkdir("-p", path.join(groupDir, "_template"));
         shell.exec("make", {
           silent: true,
-          cwd: path.join(testItemDir, "_workSpacePrepare")
+          cwd: path.join(groupDir, "_prepare")
         });
-        const itemWorkSpace = path.resolve(workSpaceDir, item.name);
+        const groupWorkspace = path.resolve(WorkspaceDir, group.name);
 
         shell
-          .ls("-l", testItemDir)
-          .filter((c, i) => c && c.name.charAt(0) != "_")
-          .forEach(c => {
-            const caseWorkSpace = path.join(itemWorkSpace, c.name);
-            shell.mkdir("-p", caseWorkSpace);
-            shell.cp(
-              "-R",
-              path.join(testItemDir, "_workSpaceTemplate/*"),
-              caseWorkSpace
-            );
+          .ls("-l", groupDir)
+          .filter((c, i) => c && c.name.charAt(0) != "_" && c.isDirectory())
+          .forEach(test => {
+            const testWorkspace = path.join(groupWorkspace, test.name);
+            shell.mkdir("-p", testWorkspace);
+            shell.cp("-r", path.join(groupDir, "_template/*"), testWorkspace);
 
             let serverPort = serverPortStart++;
             let shellServerProcess = child_process.spawn(
-              execPath,
+              ExecPath,
               [serverPort],
               {
                 silent: true,
-                cwd: caseWorkSpace
+                cwd: testWorkspace
               }
             );
 
-            let testCaseDir = path.join(testItemDir, c.name);
-            it(c.name, async () => {
-              let client = new net.Socket();
-              client.connect(serverPort, "localhost", () => {
-                client.write(fs.readFileSync(path.join(testCaseDir, "in.txt")));
-                setTimeout(() => {
-                  client.destroy();
-                }, 9000);
-              });
+            let testDir = path.join(groupDir, test.name);
+            it(test.name, async function() {
+              this.slow(10000);
+              this.timeout(0);
+              let commands = yaml.safeLoad(
+                await util.promisify(fs.readFile)(
+                  path.join(testDir, "in.yaml"),
+                  "utf8"
+                ),
+                {
+                  schema: yaml.FAILSAFE_SCHEMA
+                }
+              );
+              let answer = yaml.safeLoad(
+                await util.promisify(fs.readFile)(
+                  path.join(testDir, "out.yaml"),
+                  "utf8"
+                ),
+                {
+                  schema: yaml.FAILSAFE_SCHEMA
+                }
+              );
 
-              let shellOutput = "";
-              client.on("data", data => {
-                shellOutput += data;
-              });
+              let result = await delayClient(
+                "localhost",
+                serverPort,
+                commands,
+                CommandDelayTime
+              );
+              shellServerProcess.kill();
+              result.should.be.eql(answer);
 
-              return new Promise(resolve => {
-                client.on("close", () => {
-                  shellServerProcess.kill();
-                  shellOutput.should.be.eql(
-                    fs
-                      .readFileSync(path.join(testCaseDir, "out.txt"))
-                      .toString()
-                  );
-                  resolve();
-                });
-              });
-            }).timeout(10000);
+              // let output = yaml.safeDump(result, {
+              //   indent: 2,
+              //   lineWidth: 256,
+              //   noRefs: true,
+              //   schema: yaml.FAILSAFE_SCHEMA
+              // })
+              // await util.promisify(fs.writeFile)(path.join(testDir, "out.yaml"), output);
+            });
           });
 
         after(() => {
-          shell.rm("-Rf", itemWorkSpace);
+          shell.rm("-Rf", groupWorkspace);
         });
       });
     });
